8d9e0532629584a35de18b6d00ea48b84528576c
ewelina <skibinska.ewelina@gmail.com>
2017-02-09 11:56:49 +0000

    Yoti buttons in our colors
    related #24

---
+++ public/static/index.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+  $.getScript('static/browser.js')
+  .done(function () {
+    _ybg.config.service = 'https://www.yoti.com/connect/'
+    _ybg.init()
+    console.log( "ready!" );
+    $('.buttons a:first-of-type').css("background-color", "#25ABCA");
+    $('.buttons a:last-of-type').css("background-color", "#B3C92B");
+  })
+  .fail(function () {
+    console.log('error');
+  });
+});
