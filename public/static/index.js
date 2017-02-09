cb9cfdcc4c836f8fff9bcf1e7b40aeab38a0c1bb
Tom Barrett <tbtommyb@gmail.com>
2017-02-09 12:58:00 +0000

    Removed console.log and added semicolons etc

--- public/static/index.js
+++ public/static/index.js
@@ -1,11 +1,10 @@
 $( document ).ready(function() {
   $.getScript('static/browser.js')
   .done(function () {
-    _ybg.config.service = 'https://www.yoti.com/connect/'
-    _ybg.init()
-    console.log( "ready!" );
-    $('.buttons a:first-of-type').css("background-color", "#25ABCA");
-    $('.buttons a:last-of-type').css("background-color", "#B3C92B");
+    _ybg.config.service = 'https://www.yoti.com/connect/';
+    _ybg.init();
+    $('.buttons a:first-of-type').css('background-color', '#25ABCA');
+    $('.buttons a:last-of-type').css('background-color', '#B3C92B');
   })
   .fail(function () {
     console.log('error');
