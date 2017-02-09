1f1b5cb0d3605f31b8cc1c449c166e6d9ad305ec
emily <limeyb7@gmail.com>
2017-02-09 15:04:07 +0000

    new font and hover effect on buttons, #33

--- public/static/index.js
+++ public/static/index.js
@@ -1,10 +1,29 @@
+var blue = '#25ABCA';
+var green = '#B3C92B';
+var hoverBlue = '#30b9d9';
+var hoverGreen = '#bfd539';
+
 $( document ).ready(function() {
   $.getScript('static/browser.js')
   .done(function () {
     _ybg.config.service = 'https://www.yoti.com/connect/';
     _ybg.init();
-    $('.buttons a:first-of-type').css('background-color', '#25ABCA');
-    $('.buttons a:last-of-type').css('background-color', '#B3C92B');
+    $('.buttons a').css({
+      'font-family': 'Rubik',
+      'font-weight': 'bold',
+    });
+    $('.buttons a:first-of-type').css('background-color', blue)
+    .hover(function() {
+      $(this).css('background-color', hoverBlue);
+    }, function() {
+      $(this).css('background-color', blue);
+    });
+    $('.buttons a:last-of-type').css('background-color', green)
+    .hover(function() {
+      $(this).css('background-color', hoverGreen);
+    }, function() {
+      $(this).css('background-color', green);
+    });
   })
   .fail(function () {
     console.log('error');
